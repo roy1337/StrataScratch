7aec831e6c3cd523762f9230f05838339d2769cc
roy1337 <40725093+roy1337@users.noreply.github.com>
2022-01-11 10:30:45 +0200

    Update Marketing Campaign Success [Advanced].sql

--- Marketing Campaign Success [Advanced].sql
+++ Marketing Campaign Success [Advanced].sql
@@ -1,7 +1,7 @@
 Marketing Campaign Success [Advanced]
 You have a table of in-app purchases by user. Users that make their first in-app purchase are placed in a marketing campaign where they see call-to-actions for more in-app purchases. Find the number of users that made additional in-app purchases due to the success of the marketing campaign.
 
-The marketing campaign doesn't start until one day after the initial in-app purchase so users that make multiple purchases on the same day do not count, nor do we count users that make only the same purchases over time.
+The marketing campaign doesnt start until one day after the initial in-app purchase so users that make multiple purchases on the same day do not count, nor do we count users that make only the same purchases over time.
 
 
  select count(distinct counta.user_id) from (
