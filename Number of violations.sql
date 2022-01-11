81a3b1b07f7213cf1048ffe1729c7a4526500d18
roy1337 <40725093+roy1337@users.noreply.github.com>
2022-01-11 10:31:47 +0200

    Update Number of violations.sql

--- Number of violations.sql
+++ Number of violations.sql
@@ -1,5 +1,5 @@
 Number of violations
-You're given a dataset of health inspections. Count the number of violation in an inspection in 'Roxanne Cafe' for each year. If an inspection resulted in a violation, there will be a value in the 'violation_id' column. Output the number of violations by year in ascending order.
+Youre given a dataset of health inspections. Count the number of violation in an inspection in 'Roxanne Cafe' for each year. If an inspection resulted in a violation, there will be a value in the 'violation_id' column. Output the number of violations by year in ascending order.
 
 select count(*),extract(year from inspection_date) 
 from sf_restaurant_health_violations
