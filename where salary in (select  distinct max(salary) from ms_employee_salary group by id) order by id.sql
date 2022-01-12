986e4fde67b13e4393181dd9ba4bcd07feb8fad8
Roy <40725093+roy1337@users.noreply.github.com>
2022-01-12 15:57:25 +0200

    Create where salary in (select  distinct max(salary) from ms_employee_salary group by id) order by id.sql

---
+++ where salary in (select  distinct max(salary) from ms_employee_salary group by id) order by id.sql
@@ -0,0 +1,6 @@
+Finding Updated Records
+We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. Find the current salary of each employee assuming that salaries increase each year. Output their id, first name, last name, department ID, and current salary. Order your list by employee ID in ascending order.
+
+select distinct id,  first_name,last_name,department_id, max(salary)
+from ms_employee_salary
+group by 1,2,3,4
